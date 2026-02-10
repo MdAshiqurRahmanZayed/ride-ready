12bf3cd003b93c9fbc4677bcb531c182d2d61c8c
MdAshiqurRahmanZayed <mdzayed2019@gmail.com>
2026-02-11 02:49:53 +0600

    feat: remov acl

--- backend/RideReady/RideReady/settings.py
+++ backend/RideReady/RideReady/settings.py
@@ -151,7 +151,7 @@ if USE_S3:
     AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME", "us-east-1")
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
     AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
-    AWS_DEFAULT_ACL = "public-read"
+    AWS_DEFAULT_ACL = None  # Disable ACLs
     AWS_QUERYSTRING_AUTH = False
     AWS_S3_FILE_OVERWRITE = False
 
